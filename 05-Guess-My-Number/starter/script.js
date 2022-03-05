b70ee8a9e398fc62d99bf80335b0e8cd1213ea8f
Aldo Medina <aldo.medina.gallegos82@gmail.com>
2022-03-05 16:11:36 -0600

    logica resuelta

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,2 +1,36 @@
 'use strict';
-console.log('hello world');
+
+const secretNumber = Math.ceil(Math.random() * 20);
+console.log(secretNumber);
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = `🚫 No number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = `🥳 Correct number`;
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📉 Too Low!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `You Loose !!!`;
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = `📈 Too High!`;
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = `You Loose !!!`;
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
