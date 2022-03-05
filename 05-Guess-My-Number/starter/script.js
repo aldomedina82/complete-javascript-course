bcc1dfe0a9f42bc664e1ee25b0a50b750bdeb0e5
Aldo Medina <aldo.medina.gallegos82@gmail.com>
2022-03-05 17:15:23 -0600

    with css changes

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -4,26 +4,43 @@ const secretNumber = Math.ceil(Math.random() * 20);
 console.log(secretNumber);
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+//document.querySelector('.number').textContent = secretNumber;
+
+//Action taken with the check button
 
 document.querySelector('.check').addEventListener('click', () => {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
+  // When there is no input
   if (!guess) {
     document.querySelector('.message').textContent = `🚫 No number!`;
-  } else if (guess === secretNumber) {
+  }
+  // When you guess the number right
+  else if (guess === secretNumber) {
     document.querySelector('.message').textContent = `🥳 Correct number`;
-  } else if (guess < secretNumber) {
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.message').style.fontSize = '3rem';
+  }
+  // if the input is lower than the secret number
+  else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = `📉 Too Low!`;
       score--;
       document.querySelector('.score').textContent = score;
     } else {
       document.querySelector('.message').textContent = `You Loose !!!`;
+
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = '#d22b2b';
+      document.querySelector('.message').style.fontSize = '3rem';
     }
-  } else if (guess > secretNumber) {
+  }
+
+  // if the input is higher than the secret number
+  else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = `📈 Too High!`;
       score--;
