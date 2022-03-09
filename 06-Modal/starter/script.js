3e373fc34e2d54532a4a3d265b98a0a30f66f4fd
Aldo Medina <aldo.medina.gallegos82@gmail.com>
2022-03-08 21:04:02 -0600

    modal terminado

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,32 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btncloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+console.log(modal.classList);
+console.log(btncloseModal.classList);
+
+function openModal() {
+  overlay.classList.remove('hidden');
+  modal.classList.remove('hidden');
+}
+
+function closeModal() {
+  overlay.classList.add('hidden');
+  modal.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
+
+btncloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
